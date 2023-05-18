e6158a2da51e28d73a93272c8659ee8e7a2656f1
hyungeun.jin <kingwang09@gmail.com>
2023-05-18 13:21:56 +0900

    팩토리얼 함수 문제 풀기

--- src/main/java/goorm/level1/Factorial.java
+++ src/main/java/goorm/level1/Factorial.java
@@ -1,24 +1,37 @@
 package goorm.level1;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
 
 public class Factorial {
 
     public static void main(String[] args){
-        List<Long> values = new LinkedList<>();
-        int max = 100;
-        for(long i = 1; i <= max; i++){
-            values.add(i);
-        }
-        System.out.println(values);
-        Optional<Long> sum = values.stream().reduce((total, n) -> {
-            System.out.printf("total=%d, n=%d \n", total, n);
-            return total * n;
-        });
-        System.out.println(sum.get());
+        Answer answer = new Answer();
+        System.out.println(answer.calc(100));
     }
 
     //팩토리얼 -> 재귀써야할 것
+}
+
+class Answer{
+    private long v1 = 0;
+    private long v2 = 0;
+    private long sum = 0;
+
+    public long calc(int value){
+        long index = 2;
+        long sum = 1;
+        while(index <= value){
+            System.out.printf("sum=%d, index=%d\n", sum, index);
+            sum = sum * index;
+            index++;
+        }
+        return sum;
+    }
+
+    public int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
+        }
+    }
 }
